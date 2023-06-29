44be29b4e20022c67b66e3d1ef5a8b536d38e8a8
passion-lab <connect.subhankar@protonmail.com>
2023-06-29 23:42:58 +0530

    Notification logic added

--- main.py
+++ main.py
@@ -20,38 +20,75 @@ def power_notify(ttl: str, per: int, rem: str, msg: str, sec: int = 5):
         title=ttl,
         message=f"● {per}% battery currently\n"
                 f"● {rem} left\n"
-                f"{msg}.",
+                f"{msg}",
         app_name="System POWER",
         timeout=sec
     )
 
 
 if __name__ == '__main__':
+
+    # Will be true once notification is shown
+    is_notified = False
+
     while True:
         _battery_info = sensors_battery().power_plugged, sensors_battery().percent, \
             seconds_to_hms(sensors_battery().secsleft)
 
         # If running on battery power
         if not _battery_info[0]:
-            if _battery_info[1] <= 20:
-                pass
-            elif _battery_info[1] <= 30:
-                pass
-            elif _battery_info[1] <= 40:
-                pass
-            elif _battery_info[1] <= 60:
-                power_notify(ttl="Power Half", msg="Make sure the AC power is available",
-                             per=_battery_info[1], rem=_battery_info[2])
+
+            if _battery_info[1] < (50 or 40 or 30 or 24):
+                is_notified = False
+
+            if _battery_info[1] == 25:
+                if not is_notified:
+                    power_notify(ttl="Power Critical | URGENT", msg="Immediately plug-in the power adapter.",
+                                 per=_battery_info[1], rem=_battery_info[2], sec=10)
+                    is_notified = True
+            elif _battery_info[1] == 30:
+                if not is_notified:
+                    power_notify(ttl="Power Emergency | PLUG-IN",
+                                 msg="Device is running in emergency battery power. Connect the device with AC power "
+                                     "adapter. Shut down the system if AC not available.",
+                                 per=_battery_info[1], rem=_battery_info[2], sec=10)
+                    is_notified = True
+            elif _battery_info[1] == 40:
+                if not is_notified:
+                    power_notify(ttl="Power Down | READY", msg="Get ready to power up the battery of your device soon.",
+                                 per=_battery_info[1], rem=_battery_info[2])
+                    is_notified = True
+            elif _battery_info[1] == 50:
+                if not is_notified:
+                    power_notify(ttl="Power Half | ALERT", msg="Make sure the adapter and AC power is available.",
+                                 per=_battery_info[1], rem=_battery_info[2])
+                    is_notified = True
 
         # If running on AC power
         else:
-            if _battery_info[1] >= 80:
-                pass
-            elif _battery_info[1] >= 90:
-                pass
-            elif _battery_info[1] >= 95:
-                pass
-            elif _battery_info[1] >= 100:
-                pass
 
-        sleep(10)
+            if _battery_info[1] > (80 or 90 or 95):
+                is_notified = False
+
+            if _battery_info[1] == 80:
+                if not is_notified:
+                    power_notify(ttl="Power Enough | ALERT", msg="Now the battery power is enough to work with.",
+                                 per=_battery_info[1], rem=_battery_info[2])
+                    is_notified = True
+            elif _battery_info[1] == 90:
+                if not is_notified:
+                    power_notify(ttl="Power Limit | PLUG-OUT", msg="Release the AC adapter. Upper limit reached.",
+                                 per=_battery_info[1], rem=_battery_info[2])
+                    is_notified = True
+            elif _battery_info[1] == 95:
+                if not is_notified:
+                    power_notify(ttl="Power Over | WARNING", msg="Quickly turn off the AC power supply or"
+                                                                 " plug-out the adapter cord from the device.",
+                                 per=_battery_info[1], rem=_battery_info[2], sec=10)
+                    is_notified = True
+            elif _battery_info[1] == 100:
+                power_notify(ttl="Power Full | URGENT", msg="Battery power is full now. Switch of the AC"
+                                                            " power Immediately.",
+                             per=_battery_info[1], rem=_battery_info[2], sec=10)
+
+        sleep(60)
